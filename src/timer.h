2389ed24a5dddeced79ff8e8fd5b3f94b71ac299
skdgg <m5213jimmy@gmail.com>
2024-12-09 14:50:06 +0800

    Initial commit: Add timer header file

---
+++ src/timer.h
@@ -0,0 +1,21 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "systemc.h"
+
+SC_MODULE(timer) {
+    sc_in<bool> start;
+    sc_out<bool> timeout;
+    sc_in<bool> clock;
+    int count;
+
+    void runtimer();
+
+    SC_CTOR(timer) {
+        SC_THREAD(runtimer);
+        sensitive << clock.pos() << start;
+        count = 0;
+    }
+};
+
+#endif
